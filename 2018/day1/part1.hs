806f208c1fbaecd8f716bd4c67eccc4d58adb596
Brandon Stilson <bbstilson@fastmail.com>
2018-12-02 19:28:37 -0800

    split day 1 part 1-2

--- 2018/day1/part1.hs
+++ 2018/day1/part1.hs
@@ -1,5 +1,3 @@
-import qualified Data.Set as Set
-
 main :: IO ()
 main = do
   content <- readFile "input.txt"
@@ -12,40 +10,3 @@ parseFile = map parseInt . lines
 parseInt :: String -> Int
 parseInt (sign:n) = read num :: Int
   where num = if sign == '+' then n else sign : n
-
-main' :: IO ()
-main' = do
-  content <- readFile "input.txt"
-  let xs = parseFile content
-  let calced = reverse $ foldl calcFreqs [0] xs
-  let (seen, dupes) = findDupe calced
-
-  putStrLn $ show seen
-  putStrLn $ show dupes
-
-calcFreqs :: [Int] -> Int -> [Int]
-calcFreqs calced n = nextVal : calced
-  where
-    nextVal = head calced + n
-
-findDupe :: [Int] -> ([Int], [Int])
-findDupe xs = findDupe' 0 xs
-
-findDupe' :: Int -> [Int] -> Int
-findDupe' initVal xs =
-  case foundVal of
-    Just found -> found
-    Nothing -> findDupe' lastVal xs
-  where
-    (seen, lastVal, foundVal) = foldl (\(seen, lastVal, found) x ->
-      case found of
-        Just val -> (seen, lastVal, found)
-        Nothing -> updateState seen x
-    ) (Set.singleton, initVal, Nothing) xs
-
-updateState :: Set.Set -> Int -> (Set.Set, Int, Maybe Int)
-updateState seen x =
-  if Set.member x seen then
-    (seen, lastVal, Just x)
-  else
-    (Set.insert x seen, x, Nothing)
