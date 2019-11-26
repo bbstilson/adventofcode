d4923ff52ad003e704df83ed080def9a902d4eaa
bbstilson <bbstilson@fastmail.com>
2019-11-25 17:50:27 -0800

    2017 | day 1 and 2

---
+++ 2017/day2/Day2.scala
@@ -0,0 +1,38 @@
+import scala.io.Source
+
+object Day2 {
+  def main(args: Array[String]): Unit = {
+    val spreadsheet = parseInput()
+    println(part1(spreadsheet))
+    println(part2(spreadsheet))
+  }
+
+  def part1(spreadsheet: List[List[Int]]): Int = spreadsheet.map(getDiff).sum
+
+  def part2(spreadsheet: List[List[Int]]): Int = {
+    spreadsheet.map { xs =>
+      val pairs = for {
+        x <- xs
+        y <- xs
+        if (x != y)
+      } yield (x, y)
+
+      pairs.collectFirst {
+        case (x, y) if x % y == 0 => x / y
+      }.get
+    }.sum
+  }
+
+  def getDiff(xs: List[Int]): Int = {
+    val (min, max) = xs.foldLeft((Int.MaxValue, Int.MinValue)) {
+      case ((min, max), x) => (Math.min(min, x), Math.max(max, x))
+    }
+    max - min
+  }
+
+  private def parseInput(): List[List[Int]] = Source
+    .fromFile("day2_input.txt")
+    .getLines.toList
+    .map(_.split("\\s+").toList)
+    .map(_.map(_.toInt))
+}
