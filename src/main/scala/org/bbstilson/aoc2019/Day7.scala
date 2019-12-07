6a35b8e29d4b3d1ae6845ca01c8e915d0b492ce0
Brandon <bbstilson@fastmail.com>
2019-12-07 11:24:55 -0800

    clean up part 1 day  7

--- src/main/scala/org/bbstilson/aoc2019/Day7.scala
+++ src/main/scala/org/bbstilson/aoc2019/Day7.scala
@@ -5,17 +5,7 @@ import org.bbstilson.aoc2019.intcode.IntCodeComputer
 object Day7 {
   def main(args: Array[String]): Unit = {
     val program = parseInput("2019/day7/input.txt")
-    val testProgram = List(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0)
-
-    if (part1(program) != 87138) {
-      throw new Error("WOAH STOP")
-    }
-
-    if (part1Test(testProgram) != 43210) {
-      throw new Error("WOAH STOP")
-    }
-
-    // part2(mkMemory(input)).foreach(d => println(s"found $d"))
+    println(part1(program))
   }
 
 
@@ -32,15 +22,6 @@ object Day7 {
       IntCodeComputer(program, LazyList(phase, prev)).head
     }
   }
-
-  // def part2(program: List[Int]): LazyList[Int] = {
-  //   def a: LazyList[Int] = outputs(ProgramState(0, memory, 9 #:: 0 #:: e))
-  //   def b: LazyList[Int] = outputs(ProgramState(0, memory, 8 #:: a))
-  //   def c: LazyList[Int] = outputs(ProgramState(0, memory, 7 #:: b))
-  //   def d: LazyList[Int] = outputs(ProgramState(0, memory, 6 #:: c))
-  //   def e: LazyList[Int] = outputs(ProgramState(0, memory, 5 #:: d))
-  //   e
-  // }
 
   def parseInput(resource: String): List[Int] = {
     io.Source
