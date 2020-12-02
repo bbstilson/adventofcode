8595d71ee744e51f8b21e802b256191ccebd240f
Brandon Stilson <bbstilson@fastmail.com>
2020-12-01 22:27:52 -0800

    more readable

--- adventofcode/src/bbstilson/aoc2020/Day2.scala
+++ adventofcode/src/bbstilson/aoc2020/Day2.scala
@@ -1,33 +1,32 @@
 package bbstilson.aoc2020
 
-case class Password(min: Int, max: Int, letter: Char, pw: String)
+case class Rule(min: Int, max: Int, letter: Char, password: String)
 
 object Input {
 
-  val lineRegex = """(\d+)\-(\d+) ([a-z]): ([a-z]+)""".r
+  val LineRegex = """(\d+)\-(\d+) ([a-z]): ([a-z]+)""".r
 
-  def parseLine(str: String): Password = str match {
-    case lineRegex(min, max, char, pw) =>
-      Password(min.toInt, max.toInt, char.toCharArray().head, pw)
-    case _ => Password(0, 0, 'a', "")
+  def parseLine(str: String): Rule = str match {
+    case LineRegex(min, max, char, pw) => Rule(min.toInt, max.toInt, char.toCharArray().head, pw)
+    case _                             => Rule(0, 0, ' ', "")
   }
 }
 
 object Day2 extends bbstilson.Problem("day2", Input.parseLine) {
 
-  def run(input: List[Password]): Unit = {
+  def run(input: List[Rule]): Unit = {
     println(input.filter(isValidPart1).size)
     println(input.filter(isValidPart2).size)
   }
 
-  private def isValidPart1(pw: Password): Boolean = {
-    val numChar = pw.pw.filter(_ == pw.letter).size
-    numChar >= pw.min && numChar <= pw.max
+  private def isValidPart1(rule: Rule): Boolean = {
+    val numChar = rule.password.filter(_ == rule.letter).size
+    numChar >= rule.min && numChar <= rule.max
   }
 
-  private def isValidPart2(pw: Password): Boolean = {
-    (pw.pw(pw.min - 1) == pw.letter || pw.pw(pw.max - 1) == pw.letter) && !(pw.pw(
-      pw.min - 1
-    ) == pw.letter && pw.pw(pw.max - 1) == pw.letter)
+  private def isValidPart2(rule: Rule): Boolean = {
+    val atMin = rule.password(rule.min - 1) == rule.letter
+    val atMax = rule.password(rule.max - 1) == rule.letter
+    (atMin || atMax) && !(atMin && atMax)
   }
 }
