dd8368c9fa02253cce05ccd034bfcffee232aff6
Brandon Stilson <bbstilson@fastmail.com>
2020-12-02 21:59:20 -0800

    2020 - day 3

---
+++ adventofcode/src/bbstilson/aoc2020/Day3.scala
@@ -0,0 +1,34 @@
+package bbstilson.aoc2020
+
+object Day3 extends bbstilson.Problem("day3", identity) {
+
+  def run(input: List[String]): Unit = {
+    println(part1(input, 3, 1))
+    println(part2(input))
+  }
+
+  private def part1(input: List[String], rightStepSize: Int, downStepSize: Int): Long = {
+    (0 until input.size by downStepSize)
+      .zip((0 until (input.size * rightStepSize) by rightStepSize))
+      .drop(1)
+      .toVector
+      .map { case (level, right) =>
+        val row = input.drop(level).head
+        row(right % input.head.length())
+      }
+      .count(_ == '#')
+      .toLong
+  }
+
+  def part2(input: List[String]): Long = {
+    val combos = List(
+      (1 -> 1),
+      (3 -> 1),
+      (5 -> 1),
+      (7 -> 1),
+      (1 -> 2)
+    )
+
+    combos.map { case (right, down) => part1(input, right, down) }.product
+  }
+}
