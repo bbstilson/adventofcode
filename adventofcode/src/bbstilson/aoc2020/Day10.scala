88ce84c53e94108008021779c4d0dc8e914cf1ae
Brandon Stilson <bbstilson@fastmail.com>
2020-12-10 10:29:10 -0800

    back to original solution using memoization

--- adventofcode/src/bbstilson/aoc2020/Day10.scala
+++ adventofcode/src/bbstilson/aoc2020/Day10.scala
@@ -11,46 +11,36 @@ object Day10 extends aocd.Problem(2020, 10) {
     println(part2(fixedAdapters))
   }
 
-  case class Joltages(ones: Int, threes: Int) {
-    def result: Int = ones * threes
-  }
-
-  def part1(adapters: List[Int]): Int =
-    adapters
+  def part1(adapters: List[Int]): Int = {
+    val (ones, threes) = adapters
       .sliding(2)
       .collect { case List(a, b) => b - a }
-      .foldLeft(Joltages(0, 0)) { case (js, i) =>
+      .foldLeft((0, 0)) { case ((ones, threes), i) =>
         i match {
-          case 1 => js.copy(ones = js.ones + 1)
-          case 3 => js.copy(threes = js.threes + 1)
+          case 1 => (ones + 1, threes)
+          case 3 => (ones, threes + 1)
         }
       }
-      .result
+    ones * threes
+  }
 
   def part2(adapters: List[Int]): Long = {
-    implicit val graph = buildGraph(adapters, Map.empty)
-    val intersections = buildIntersections(adapters)
-    val merged = mergeIntersections(intersections)
-    val permutations = merged.map(intersectionPermutations)
-    permutations.product
-  }
-
-  def mergeIntersections(intersections: List[List[Int]]): List[List[Int]] = {
-    val (out, carry) = intersections.tail.foldLeft((List.empty[List[Int]], intersections.head)) {
-      case ((out, carry), intersection) =>
-        if (intersection.head > carry.head && intersection.head < carry.last) {
-          (out, carry ++ intersection)
-        } else {
-          (out :+ carry, intersection)
+    val graph = buildGraph(adapters, Map.empty)
+    val cache = collection.mutable.Map.empty[Int, Long]
+    def traverseGraph(node: Int): Long = cache.getOrElse(
+      node, {
+        graph.get(node) match {
+          case Some(edges) => {
+            cache.update(node, edges.map(traverseGraph).sum)
+            cache(node)
+          }
+          case None => 1L
         }
+      }
+    )
 
-    }
-    (out :+ carry).map(_.distinct)
+    traverseGraph(0)
   }
-
-  def buildIntersections(adapters: List[Int])(implicit
-    graph: Map[Int, List[Int]]
-  ): List[List[Int]] = adapters.map(buildIntersection).filter(_.size > 2)
 
   @tailrec
   def buildGraph(adapters: List[Int], graph: Map[Int, List[Int]]): Map[Int, List[Int]] =
@@ -58,30 +48,4 @@ object Day10 extends aocd.Problem(2020, 10) {
       case hd :: tl if tl.nonEmpty => buildGraph(tl, (graph + (hd -> tl.takeWhile(_ - hd <= 3))))
       case _                       => graph
     }
-
-  def buildIntersection(start: Int)(implicit graph: Map[Int, List[Int]]): List[Int] =
-    graph.get(start) match {
-      case Some(edges) if edges.size > 1 =>
-        List(start) ++ edges.init ++ buildIntersection(edges.last)
-      case Some(List(edge)) => List(start, edge)
-      case _                => Nil
-    }
-
-  def intersectionPermutations(xs: List[Int]): Long = {
-    val (start, end) = (xs.head, xs.last)
-    xs.permutations
-      .map(
-        _.sliding(2)
-          .takeWhile {
-            case List(a, b) => a < b && b - a <= 3
-            case _          => false // silence compiler
-          }
-          .toList
-          .flatten
-      )
-      .toList
-      .distinct
-      .count(xs => xs.nonEmpty && xs.head == start && xs.last == end)
-      .toLong
-  }
 }
