25b4052fee2daa4ff4598358ad292e5ee2c62eaf
Brandon Stilson <bbgithub@fastmail.com>
2023-12-02 22:10:33 -0800

    part 2

--- src/y23/d3.rs
+++ src/y23/d3.rs
@@ -12,22 +12,10 @@ pub struct Day;
 impl AdventOfCode for Day {
     fn solve() -> Result<()> {
         let input = Day::input_lines(2023, 3)?;
-        //         let input = "467..114..
-        // ...*......
-        // ..35..633.
-        // ......#...
-        // 617*......
-        // .....+.58.
-        // ..592.....
-        // ......755.
-        // ...$.*....
-        // .664.598.."
-        //             .split('\n')
-        //             .map(|l| l.into())
-        //             .collect();
         let schematic = parse_schematic(input)?;
 
         println!("{}", part_1(&schematic));
+        println!("{}", part_2(&schematic));
 
         Ok(())
     }
@@ -47,7 +35,7 @@ struct Symbol {
 
 #[derive(Debug)]
 struct Number {
-    n: isize,
+    n: usize,
     pos: (isize, std::ops::Range<usize>),
 }
 
@@ -70,7 +58,7 @@ fn parse_schematic(lines: Vec<String>) -> Result<Schematic> {
         for cap in NUMBERS_REGEX.captures_iter(&line) {
             let re_match = cap.get(0).unwrap();
             numbers.push(Number {
-                n: re_match.as_str().parse::<isize>()?,
+                n: re_match.as_str().parse::<usize>()?,
                 pos: (row_idx as isize, re_match.range()),
             });
         }
@@ -79,40 +67,60 @@ fn parse_schematic(lines: Vec<String>) -> Result<Schematic> {
     Ok(Schematic { symbols, numbers })
 }
 
-fn part_1(schematic: &Schematic) -> isize {
+fn part_1(schematic: &Schematic) -> usize {
     // for each symbol, find any numbers on the same row or +- 1 row away.
     // for each found number, check if the range is +-1 away from the symbol column
-
     schematic
         .symbols
         .iter()
-        .map(|symbol| {
-            schematic
-                .numbers
-                .iter()
-                .filter(|number| {
-                    // get numbers that are:
-                    // a) on the same row, or
-                    // b) one row up or down (for diagonal checks)
-                    let number_row = number.pos.0;
-                    let symbol_row = symbol.pos.0;
-                    number_row == symbol_row
-                        || number_row - 1 == symbol_row
-                        || number_row + 1 == symbol_row
-                })
-                .filter(|number| {
-                    // check if the symbol position is overlapping the number range
-                    // accounting for diagonals (+-1)
-                    let number_range = &number.pos.1;
-                    let symbol_col = symbol.pos.1;
-                    let left_shift = (symbol_col - 1) as usize;
-                    let right_shift = (symbol_col + 1) as usize;
-                    number_range.contains(&right_shift)
-                        || number_range.contains(&left_shift)
-                        || number_range.contains(&(symbol_col as usize))
-                })
-                .map(|number| number.n)
-                .sum::<isize>()
+        .map(|symbol| get_adjacent_numbers(symbol, &schematic.numbers).sum::<usize>())
+        .sum()
+}
+
+fn part_2(schematic: &Schematic) -> usize {
+    // find all gears (symbols == '*')
+    // for each gear, find any numbers on the same row or +- 1 row away.
+    // if there are exactly 2 numbers, get their product
+    schematic
+        .symbols
+        .iter()
+        .filter(|symbol| symbol.c == '*')
+        .filter_map(|symbol| {
+            let gears = get_adjacent_numbers(symbol, &schematic.numbers).collect::<Vec<_>>();
+
+            if gears.len() == 2 {
+                Some(gears.iter().product::<usize>())
+            } else {
+                None
+            }
         })
         .sum()
+}
+
+fn get_adjacent_numbers<'a>(
+    symbol: &'a Symbol,
+    numbers: &'a Vec<Number>,
+) -> impl Iterator<Item = usize> + 'a {
+    numbers
+        .iter()
+        .filter(|number| {
+            // get numbers that are:
+            // a) on the same row, or
+            // b) one row up or down (for diagonal checks)
+            let number_row = number.pos.0;
+            let symbol_row = symbol.pos.0;
+            number_row == symbol_row || number_row - 1 == symbol_row || number_row + 1 == symbol_row
+        })
+        .filter(|number| {
+            // check if the symbol position is overlapping the number range
+            // accounting for diagonals (+-1)
+            let number_range = &number.pos.1;
+            let symbol_col = symbol.pos.1;
+            let left_shift = (symbol_col - 1) as usize;
+            let right_shift = (symbol_col + 1) as usize;
+            number_range.contains(&right_shift)
+                || number_range.contains(&left_shift)
+                || number_range.contains(&(symbol_col as usize))
+        })
+        .map(|number| number.n)
 }
