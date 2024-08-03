4b7ab9b40c4d5bdc6e2cd4371feaec8c3b7eb92c
Brandon Stilson <bbgithub@fastmail.com>
2024-08-03 10:25:43 -0700

    tidy

--- src/y15/d23.rs
+++ src/y15/d23.rs
@@ -31,13 +31,6 @@ impl Day {
     }
 }
 
-// hlf r sets register r to half its current value, then continues with the next instruction.
-// tpl r sets register r to triple its current value, then continues with the next instruction.
-// inc r increments register r, adding 1 to it, then continues with the next instruction.
-// jmp offset is a jump; it continues with the instruction offset away relative to itself.
-// jie r, offset is like jmp, but only jumps if register r is even ("jump if even").
-// jio r, offset is like jmp, but only jumps if register r is 1 ("jump if one", not odd).
-
 #[derive(Debug)]
 enum Instruction {
     Hlf(char),
